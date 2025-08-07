2ebf696d87bb90f94c8d09296aacf31688742e2e
Kashish Sharma <ks2012150@gmail.com>
2025-08-07 13:49:09 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -23,3 +23,6 @@ function App() {
 }
 
 export default App;
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+console.log("BharatVerse loaded.");
+function toggleModal(id) {
+  const modal = document.getElementById(id);
+  modal.style.display = modal.style.display === "flex" ? "none" : "flex";
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function signUp() {
+  const name = document.getElementById('signup-name').value;
+  const email = document.getElementById('signup-email').value;
+  const password = document.getElementById('signup-password').value;
+  alert(`Registered:\nName: ${name}\nEmail: ${email}`);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function signIn() {
+  const email = document.getElementById('signin-email').value;
+  const password = document.getElementById('signin-password').value;
+  alert(`Logged in as: ${email}`);
+}
+
+function subscribePayment() {
+  const email = document.getElementById('subscribe-email').value;
+  alert(`Initiating ₹199 payment for: ${email}`);
+  // Razorpay or Stripe integration comes here
+}
+
